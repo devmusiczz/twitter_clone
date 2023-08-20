10ce11da7d61a664990f11f513df345d9adc10b8
Dev Rathore <onlyfordance0@gmail.com>
2023-08-20 19:16:02 +0530

    adding required svg and theme

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants {
+  static const String databaseId = "64e15ca8eadd20dad08f";
+  static const String projectId = "64e153031f2ca3aa0544";
+  static const String endPoint = 'http://localhost' ;
+  
+}
